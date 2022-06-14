636f8813ebfcf08b5448c5894687d3607a47f72b
Cinquantesix <hello@cinquantesix.ch>
2022-06-14 06:57:33 +0200

    JS setup

---
+++ main.js
@@ -0,0 +1,4 @@
+let feedbackCard = document.querySelector('.feedback-card-rating');
+let feedbackCardWidth = feedbackCard.offsetWidth;
+
+document.querySelector(".feedback-card-rating").style.height = feedbackCardWidth+"px";
