8ea2c98d25ef1ed8ca77546e209c30fc766323d1
Cinquantesix <hello@cinquantesix.ch>
2022-06-14 17:42:11 +0200

    Add a function that set height on the rating input

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 let feedbackCard = document.querySelector('.feedback-card-rating');
-let feedbackCardWidth = feedbackCard.offsetWidth;
+let feedbackCardWidth = feedbackCard.Width;
 
 document.querySelector(".feedback-card-rating").style.height = feedbackCardWidth+"px";
+
+let feedbackCardInputRadio = document.querySelector('.radio-rating');
+let feedbackCardWidthInputRadio = feedbackCardInputRadio.offsetWidth;
+
+document.querySelector(".radio-rating").style.height = feedbackCardWidthInputRadio+"px";
+
+var RadioHeight = document.getElementsByClassName("radio-rating");
+
+for(var i=0;i<RadioHeight.length;i++){
+    RadioHeight[i].style.height = feedbackCardWidthInputRadio+"px";
+}
