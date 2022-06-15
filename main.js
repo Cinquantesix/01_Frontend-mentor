f2f866485a44d12262a15efddaefcdb5a76e2853
Cinquantesix <hello@cinquantesix.ch>
2022-06-15 11:36:41 +0200

    Add Full Size height for positioning and Button event listener

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let sizeCard = document.querySelector('.feedback-card-rating');
+let SizeCardWrapper = sizeCard.offsetWidth;
+document.getElementById("wrapper").style.height = SizeCardWrapper+"px"; 
+
 let feedbackCardInputRadio = document.querySelector('.radio-rating');
 let feedbackCardWidthInputRadio = feedbackCardInputRadio.offsetWidth;
 
@@ -6,3 +10,14 @@ var RadioHeight = document.getElementsByClassName("radio-rating");
 for(var i=0;i<RadioHeight.length;i++){
     RadioHeight[i].style.height = feedbackCardWidthInputRadio+"px";
 }
+
+// selecting the element
+let button = document.querySelector('#submit');
+
+// Add class to the element
+button.addEventListener('click', function() {
+  var x = document.querySelector('.feedback-card-rating');
+  x.classList.add('flip');
+  var y = document.querySelector('.feedback-card-state')
+  y.classList.add('flip');
+});
