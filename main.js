1dcda9c8a57a9a6a40698f6785f955ed2b56863f
Cinquantesix <hello@cinquantesix.ch>
2022-06-15 09:08:30 +0200

    Add SVG to card state

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-let feedbackCard = document.querySelector('.feedback-card-rating');
-let feedbackCardWidth = feedbackCard.offsetWidth;
-let Finalwidth = Number(feedbackCardWidth)-100;
-
-document.querySelector(".feedback-card-rating").style.height = Finalwidth+"px";
-
 let feedbackCardInputRadio = document.querySelector('.radio-rating');
 let feedbackCardWidthInputRadio = feedbackCardInputRadio.offsetWidth;
 
