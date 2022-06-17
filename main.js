508245731845d34023285d863e2f989e0dc801b5
Cinquantesix <hello@cinquantesix.ch>
2022-06-17 08:41:55 +0200

    Add the radio rating listener

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let sizeCard = document.querySelector('.feedback-card-rating');
 let SizeCardWrapper = sizeCard.offsetWidth;
-document.getElementById("wrapper").style.height = SizeCardWrapper+"px"; 
+document.getElementById("wrapper").style.height = SizeCardWrapper+"px";
 
 let feedbackCardInputRadio = document.querySelector('.radio-rating');
 let feedbackCardWidthInputRadio = feedbackCardInputRadio.offsetWidth;
@@ -16,8 +16,21 @@ let button = document.querySelector('#submit');
 
 // Add class to the element
 button.addEventListener('click', function() {
-  var x = document.querySelector('.feedback-card-rating');
-  x.classList.add('flip');
-  var y = document.querySelector('.feedback-card-state')
-  y.classList.add('flip');
+
+  var checkRadio = document.querySelector('input[name="card-rating"]:checked');
+
+  if(checkRadio != null) {
+    document.getElementById("Checked-value").innerHTML = checkRadio.value;
+
+    var x = document.querySelector('.feedback-card-rating');
+    x.classList.add('flip');
+    
+    var y = document.querySelector('.feedback-card-state')
+    y.classList.add('flip');
+  }
+
+  else {
+    alert("Please, make sure to choose a raing.");
+  }
+
 });
